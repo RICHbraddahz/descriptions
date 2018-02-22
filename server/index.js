ac3774f435e89ae9437c240c9fef43b373b1992c
James Powers <powersajames@gmail.com>
2018-02-22 14:23:40 -0800

    initial setup

--- server/index.js
+++ server/index.js
@@ -4,12 +4,11 @@ const promise = require('bluebird');
 const request = require('request-promise');
 const Repo = require('../database');
 let app = express();
-
-app.get('/', (req, res) => {
-
-});
-
 let port = 3001;
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+
 
 app.listen(port, function() {
   console.log(`listening on port ${port}`);
