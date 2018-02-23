b3303296dff975de3baee090520ea0c08cba7bf2
James Powers <powersajames@gmail.com>
2018-02-23 13:49:52 -0800

    added schema

--- database/index.js
+++ database/index.js
@@ -2,7 +2,45 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/amenities');
 
 let repoSchema = mongoose.Schema({
-
+  id: '$oid',
+  user: {
+    name: 'string',
+    thumbnail: 'string',
+    link: 'string',
+  },
+  shipDetails: {
+    capacity: 'number',
+    boatRules: 'array',
+    heads: 'number',
+    description: 'string',
+    bedrooms: {
+      bedrooms: 'number',
+      sleepingArangement: 'array',
+    },
+    amenities: {
+      priority: {
+        anchor: 'boolean',
+        engine: 'boolean',
+        lifeJacket: 'boolean',
+        twoWayRadio: 'boolean',
+        soundSystem: 'boolean',
+        tv: 'boolean',
+        kitchen: 'boolean',
+        ac: 'boolean',
+        heating: 'boolean',
+      },
+      optional: {
+        inflatables: 'boolean',
+        fishingGear: 'boolean',
+        scubaGear: 'boolean',
+        harpoons: 'boolean',
+        sharkCage: 'boolean',
+        medication: 'boolean',
+        wifi: 'boolean',
+        pool: 'boolean',
+      },
+    }
+  },
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
