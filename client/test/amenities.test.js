09048de9a9ba187721032b5b7db36e16638f8318
James Powers <powersajames@gmail.com>
2018-02-23 15:17:36 -0800

    added jest/enzyme and test file into client

---
+++ client/test/amenities.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { shallow, mount, render } from 'enzyme';
+import amenities from './../components/amenities';
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
