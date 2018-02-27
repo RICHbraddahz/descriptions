514c98d3fb3c1bc16dfc240ddcdf19784b32e67a
James Powers <powersajames@gmail.com>
2018-02-27 15:17:45 -0800

    created tests/ implemented mvp boatRules

--- client/components/boatRules.jsx
+++ client/components/boatRules.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import BoatRule from './boatRule.jsx';
 
-const HouseRules = (props) => (
-
+const BoatRules = ( {boatRules} ) => (
+  <div className="boat-rules">
+    <div>
+      {
+        boatRules.map(boatRule => (
+          <div><BoatRule
+            boatRule={boatRule}
+          /></div>));
+        )
+      }
+    </div>
+  </div>
 )
 
-export default HouseRules;
+export default BoatRules;
