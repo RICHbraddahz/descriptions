77ba71bf49ab2405b165777bc679fadb5935562b
kguinto <klogold1@gmail.com>
2018-03-23 15:36:14 -0700

    Refactored logging to use progress bars

--- benchmarking/benchmarker.mongo.js
+++ benchmarking/benchmarker.mongo.js
@@ -1,12 +1,13 @@
 /* eslint-disable no-await-in-loop */
 const { MongoClient } = require('mongodb');
 const dateMath = require('date-arithmetic');
+const ProgressBar = require('ascii-progress');
 
 const url = process.env.url || 'mongodb://localhost:27017';
 const dbName = process.env.dbname || 'descriptions';
 const startId = process.env.startId || 0;
 const endId = process.env.endId || 10000000;
-const idCount = process.env.idCount || 100000;
+const idCount = process.env.idCount || 5000;
 
 let average = array => (array.reduce((acc, val) => acc + val)) / array.length;
 
@@ -18,19 +19,28 @@ let runBenchmark = async () => {
   let startTime = new Date();
   console.log('/* -----------------------');
   console.log('| Starting benchmark.');
-  console.log('| Testing: MongoDB with id as index '); // Change this for different configurations
+  console.log('| Testing: MongoDB, stringified with id as index '); // Change this for different configurations
   console.log(`| ${idCount} ids from ${startId} to ${endId}`);
   console.log(`| Start time: ${startTime}`);
   console.log('+ ------------------------');
 
+  let progressBar = new ProgressBar({
+    schema: `| [:bar] :percent (:i/${idCount} reads)\n| Found id :foundId in :readTime seconds`,
+    total: idCount
+  });
+
   let readTimes = [];
 
-  for (let i = startId; i < endId; i += Math.round((endId - startId) / idCount)) {
+  let increment = Math.round((endId - startId) / idCount);
+  let randId = i => Math.floor((Math.random() * (increment)) + i);
+
+  for (let i = startId; i < endId; i += increment) {
     let readStartTime = new Date();
-    await collection.findOne({ id: i });
+    let id = randId(i);
+    await collection.findOne({ id });
     let readTime = dateMath.diff(readStartTime, new Date(), 'seconds', true);
     readTimes.push(readTime);
-    console.log(`| Found id ${i} in ${readTime} seconds`);
+    progressBar.tick({ i: readTimes.length, foundId: id, readTime });
   }
 
   let endTime = new Date();
