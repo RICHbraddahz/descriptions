40e04b94fbd19721c98a2730c9b55195b81d3861
kguinto <klogold1@gmail.com>
2018-03-28 11:53:37 -0700

    Combined getData and renderComponent to single file. Refactored to cache rendered html, rather than raw data

--- server/renderComponent.js
+++ server/renderComponent.js
@@ -1,7 +1,25 @@
 const App = require('../client/dist/bundle').default;
 const React = require('react');
 const ReactDOM = require('react-dom/server');
+const { getDescriptionById } = require('../database/models/descriptionModel.mongo');
 
-const renderComponent = data => ReactDOM.renderToString(React.createElement(App, { data }));
+const getCachedData = async (id, redisClient) => redisClient.getAsync(id);
 
-module.exports = renderComponent;
+const getRenderedComponent = async (id, mongoClient, redisClient) => {
+  const cachedHtml = getCachedData(id, redisClient);
+  if (cachedHtml) {
+    return cachedHtml;
+  }
+
+  const db = mongoClient.db('descriptions_n');
+  const collection = db.collection('descriptions_n');
+
+  const itemData = await getDescriptionById(collection, id);
+
+  const html = ReactDOM.renderToString(React.createElement(App, { data: itemData }));
+  redisClient.setAsync(id, html);
+
+  return html;
+};
+
+module.exports = getRenderedComponent;
